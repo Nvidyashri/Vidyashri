7da0b1026aaf65bcb800ffe159fd47994cb6dadc
unknown <nesrekarvidya.xworkz@gmail.com>
2022-08-20 00:53:07 +0530

    Inheritence_19aug

---
+++ 19-08-2022/inheritence_19/src/com/xworkz/inheritence_19/sup/Market.java
@@ -0,0 +1,26 @@
+package com.xworkz.inheritence_19.sup;
+
+public class Market {
+	
+	public String typeOfMarket;
+	public double openTime;
+	public double closeTime;
+	
+	public Market(String typeOfMarket,double openTime,double closeTime) {
+
+		this.typeOfMarket = typeOfMarket;
+		this.openTime = openTime;
+		this.closeTime = closeTime;
+		System.out.println("Super Class Constructor.");
+	}
+	public void display() {
+		
+		System.out.println(this.typeOfMarket);
+		System.out.println(this.openTime);
+		System.out.println(this.closeTime);
+
+	}
+
+}
+
+
