b80d6d31ca55531a5651bfe88dd49ff12e896e03
unknown <nesrekarvidya.xworkz@gmail.com>
2022-08-20 22:03:00 +0530

    message

---
+++ 1-08-2022/IceCreamRunner.java
@@ -0,0 +1,217 @@
+class IceCreamRunner{
+	
+	public static void main(String[] args){
+		
+		IceCream iceCream=new IceCream();
+		iceCream.flavour="Chocalate";
+		iceCream.price=160;
+		iceCream.type="Cup";
+		iceCream.color="Dark Brown";
+		iceCream.brand="Cornetto";
+		iceCream.weight=0.5;
+		iceCream.quantity=0.50;
+		iceCream.dryFruit=true;
+		iceCream.goodTaste=true;
+		iceCream.since=1997;
+		System.out.println(iceCream.flavour);
+		System.out.println(iceCream.price);
+	    System.out.println(iceCream.type);
+		System.out.println(iceCream.color);
+		System.out.println(iceCream.brand);
+		System.out.println(iceCream.weight);
+		System.out.println(iceCream.quantity);
+		System.out.println(iceCream.dryFruit);
+		System.out.println(iceCream.goodTaste);
+		System.out.println(iceCream.since);
+		System.out.println("==============================");
+		IceCream iceCream1=new IceCream("Chocalate");
+		iceCream1.price=160;
+		iceCream1.type="Cup";
+		iceCream1.color="Dark Brown";
+		iceCream1.brand="Cornetto";
+		iceCream1.weight=0.5;
+		iceCream1.quantity=0.50;
+		iceCream1.dryFruit=true;
+		iceCream1.goodTaste=true;
+		iceCream1.since=1997;
+		System.out.println(iceCream1.flavour);
+		System.out.println(iceCream1.price);
+	    System.out.println(iceCream1.type);
+		System.out.println(iceCream1.color);
+		System.out.println(iceCream1.brand);
+		System.out.println(iceCream1.weight);
+		System.out.println(iceCream1.quantity);
+		System.out.println(iceCream1.dryFruit);
+		System.out.println(iceCream1.goodTaste);
+		System.out.println(iceCream1.since);
+		System.out.println("==============================");
+		IceCream iceCream2=new IceCream("Chocalate",160);
+		iceCream2.type="Cup";
+		iceCream2.color="Dark Brown";
+		iceCream2.brand="Cornetto";
+		iceCream2.weight=0.5;
+		iceCream2.quantity=0.50;
+		iceCream2.dryFruit=true;
+		iceCream2.goodTaste=true;
+		iceCream2.since=1997;
+		System.out.println(iceCream2.flavour);
+		System.out.println(iceCream2.price);
+	    System.out.println(iceCream2.type);
+		System.out.println(iceCream2.color);
+		System.out.println(iceCream2.brand);
+		System.out.println(iceCream2.weight);
+		System.out.println(iceCream2.quantity);
+		System.out.println(iceCream2.dryFruit);
+		System.out.println(iceCream2.goodTaste);
+		System.out.println(iceCream2.since);
+		System.out.println("==============================");
+		IceCream iceCream3=new IceCream("Chocalate",160,"Cup");
+		iceCream3.color="Dark Brown";
+		iceCream3.brand="Cornetto";
+		iceCream3.weight=0.5;
+		iceCream3.quantity=0.50;
+		iceCream3.dryFruit=true;
+		iceCream3.goodTaste=true;
+		iceCream3.since=1997;
+		System.out.println(iceCream3.flavour);
+		System.out.println(iceCream3.price);
+	    System.out.println(iceCream3.type);
+		System.out.println(iceCream3.color);
+		System.out.println(iceCream3.brand);
+		System.out.println(iceCream3.weight);
+		System.out.println(iceCream3.quantity);
+		System.out.println(iceCream3.dryFruit);
+		System.out.println(iceCream3.goodTaste);
+		System.out.println(iceCream3.since);
+		System.out.println("==============================");
+		IceCream iceCream4=new IceCream("Chocalate",160,"Cup","Dark Brown");
+		iceCream4.brand="Cornetto";
+		iceCream4.weight=0.5;
+		iceCream4.quantity=0.50;
+		iceCream4.dryFruit=true;
+		iceCream4.goodTaste=true;
+		iceCream4.since=1997;
+		System.out.println(iceCream4.flavour);
+		System.out.println(iceCream4.price);
+	    System.out.println(iceCream4.type);
+		System.out.println(iceCream4.color);
+		System.out.println(iceCream4.brand);
+		System.out.println(iceCream4.weight);
+		System.out.println(iceCream4.quantity);
+		System.out.println(iceCream4.dryFruit);
+		System.out.println(iceCream4.goodTaste);
+		System.out.println(iceCream4.since);
+		System.out.println("==============================");
+		IceCream iceCream5=new IceCream("Chocalate",160,"Cup","Dark Brown","Cornetto");
+		iceCream5.weight=0.5;
+		iceCream5.quantity=0.50;
+		iceCream5.dryFruit=true;
+		iceCream5.goodTaste=true;
+		iceCream5.since=1997;
+		System.out.println(iceCream5.flavour);
+		System.out.println(iceCream5.price);
+	    System.out.println(iceCream5.type);
+		System.out.println(iceCream5.color);
+		System.out.println(iceCream5.brand);
+		System.out.println(iceCream5.weight);
+		System.out.println(iceCream5.quantity);
+		System.out.println(iceCream5.dryFruit);
+		System.out.println(iceCream5.goodTaste);
+		System.out.println(iceCream5.since);
+		System.out.println("==============================");
+		IceCream iceCream6=new IceCream("Chocalate",160,"Cup","Dark Brown","Cornetto",0.5);
+		iceCream6.quantity=0.50;
+		iceCream6.dryFruit=true;
+		iceCream6.goodTaste=true;
+		iceCream6.since=1997;
+		System.out.println(iceCream6.flavour);
+		System.out.println(iceCream6.price);
+	    System.out.println(iceCream6.type);
+		System.out.println(iceCream6.color);
+		System.out.println(iceCream6.brand);
+		System.out.println(iceCream6.weight);
+		System.out.println(iceCream6.quantity);
+		System.out.println(iceCream6.dryFruit);
+		System.out.println(iceCream6.goodTaste);
+		System.out.println(iceCream6.since);
+		System.out.println("==============================");
+		IceCream iceCream7=new IceCream("Chocalate",160,"Cup","Dark Brown","Cornetto",0.5,0.50);
+		iceCream7.dryFruit=true;
+		iceCream7.goodTaste=true;
+		iceCream7.since=1997;
+		System.out.println(iceCream7.flavour);
+		System.out.println(iceCream7.price);
+	    System.out.println(iceCream7.type);
+		System.out.println(iceCream7.color);
+		System.out.println(iceCream7.brand);
+		System.out.println(iceCream7.weight);
+		System.out.println(iceCream7.quantity);
+		System.out.println(iceCream7.dryFruit);
+		System.out.println(iceCream7.goodTaste);
+		System.out.println(iceCream7.since);
+		System.out.println("==============================");
+		IceCream iceCream8=new IceCream("Chocalate",160,"Cup","Dark Brown","Cornetto",0.5,0.50,true);
+		iceCream8.goodTaste=true;
+		iceCream8.since=1997;
+		System.out.println(iceCream8.flavour);
+		System.out.println(iceCream8.price);
+	    System.out.println(iceCream8.type);
+		System.out.println(iceCream8.color);
+		System.out.println(iceCream8.brand);
+		System.out.println(iceCream8.weight);
+		System.out.println(iceCream8.quantity);
+		System.out.println(iceCream8.dryFruit);
+		System.out.println(iceCream8.goodTaste);
+		System.out.println(iceCream8.since);
+		System.out.println("==============================");
+		IceCream iceCream9=new IceCream("Chocalate",160,"Cup","Dark Brown","Cornetto",0.5,0.50,true,true);
+		iceCream8.since=1997;
+		System.out.println(iceCream9.flavour);
+		System.out.println(iceCream9.price);
+	    System.out.println(iceCream9.type);
+		System.out.println(iceCream9.color);
+		System.out.println(iceCream9.brand);
+		System.out.println(iceCream9.weight);
+		System.out.println(iceCream9.quantity);
+		System.out.println(iceCream9.dryFruit);
+		System.out.println(iceCream9.goodTaste);
+		System.out.println(iceCream9.since);
+		System.out.println("==============================");
+		IceCream iceCream10=new IceCream("Chocalate",160,"Cup","Dark Brown","Cornetto",0.5,0.50,true,true,1997);
+		System.out.println(iceCream10.flavour);
+		System.out.println(iceCream10.price);
+	    System.out.println(iceCream10.type);
+		System.out.println(iceCream10.color);
+		System.out.println(iceCream10.brand);
+		System.out.println(iceCream10.weight);
+		System.out.println(iceCream10.quantity);
+		System.out.println(iceCream10.dryFruit);
+		System.out.println(iceCream10.goodTaste);
+		System.out.println(iceCream10.since);
+		System.out.println("==============================");
+		
+		
+	}
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+										
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
