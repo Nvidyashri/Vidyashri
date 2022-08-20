87a50ae228e5285fec7a940f97ffb2f420dbb02e
unknown <nesrekarvidya.xworkz@gmail.com>
2022-08-20 22:38:24 +0530

    message

---
+++ 5-7-2022/MobileNo.java
@@ -0,0 +1,29 @@
+class MobileNo{
+public static void main(String[] phone){
+	long prashantMobileNo=7676487629L;
+	long pacchuuMobileNo=7019572385L;
+	long appajiMobileNo=9632530286L;
+	long mummyMobileNo=8792725331L;
+	long sampatMobileNo=9732977682L;
+	long rajuMobileNo=9980977560L;
+	long kamalaMobileNo=9980977618L;
+	long manjulhaMobileNo=8762980820L;
+	long apekshaMobileNo=7795139408L;
+	long ajayMobileNo=7022476239L;
+	
+	long[] numbers={prashantMobileNo,pacchuuMobileNo,appajiMobileNo,mummyMobileNo,sampatMobileNo,rajuMobileNo,kamalaMobileNo,
+	manjulhaMobileNo,apekshaMobileNo,ajayMobileNo};
+	
+	System.out.println(numbers[0]);
+	System.out.println(numbers[1]);
+	System.out.println(numbers[2]);
+	System.out.println(numbers[3]);
+	System.out.println(numbers[4]);
+	System.out.println(numbers[5]);
+	System.out.println(numbers[6]);
+	System.out.println(numbers[7]);
+	System.out.println(numbers[8]);
+	System.out.println(numbers[9]);
+}
+}
+	
