dc4e0c0a99727b8834fea1d83d6492c347614c61
unknown <nesrekarvidya.xworkz@gmail.com>
2022-08-24 13:27:30 +0530

    test orogram (Revers)

---
+++ reversRunner/src/com/xworkz/reversRunner/ReversRunner.java
@@ -0,0 +1,17 @@
+package com.xworkz.reversRunner;
+
+public class ReversRunner {
+
+	public static void main(String[] args) {
+        String name="test";
+		
+		for(int vidya=name.length();vidya>=0;vidya--) {
+			
+			
+			System.out.println("print name as vidya");
+		}
+		
+
+	}
+
+}
