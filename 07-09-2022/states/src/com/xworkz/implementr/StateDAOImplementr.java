1fe96d94177be5bba96e2290917e33d1c784d259
unknown <nesrekarvidya.xworkz@gmail.com>
2022-09-07 22:34:12 +0530

    DAO Example

--- 07-09-2022/states/src/com/xworkz/implementr/StateDAOImplementr.java
+++ 07-09-2022/states/src/com/xworkz/implementr/StateDAOImplementr.java
@@ -2,27 +2,30 @@ package com.xworkz.implementr;
 
 import com.xworkz.javadao.dao.StatesDAO;
 
-public class StateDAOImplementr implements StatesDAO {
+public class StateDAOImplementr implements StatesDAO{
 	
 	private String[] stateNames = new String[20];
 	
-     @Override
-    public boolean save(String StateNames) {
-    	 for(int i=0;i < stateNames.length; i++) {
-    		 if(stateNames[i]==null) {
-    			 stateNames[i]=StateNames;
-    			 break;
-    			 
-    			 
-    		 }
-    	 }
-    	return false;
-    }
-     @Override
-    public void printlnAllStates() {
-    	 for(int i=0; i<stateNames.length; i++) {
-    		 System.out.println(stateNames[i]);
-    	 }
-    	
-    }
+    @Override
+   public boolean save(String StateNames) {
+   	 for(int i=0;i < stateNames.length; i++) {
+   		 if(stateNames[i]==null) {
+   			 stateNames[i]=StateNames;
+   			 break;
+   			 
+   			 
+   		 }
+   	 }
+   	return false;
+   }
+    @Override
+   public void printlnAllStates() {
+   	 for(int i=0; i<stateNames.length; i++) {
+   		 System.out.println(stateNames[i]);
+   	 }
+   	
+   }
+	
+	
+
 }
--- 07-09-2022/states/src/com/xworkz/javadao/StatesElite.java
+++ 07-09-2022/states/src/com/xworkz/javadao/StatesElite.java
@@ -5,33 +5,31 @@ import com.xworkz.implementr.StateDAOImplementr;
 public class StatesElite {
 
 	public static void main(String[] args) {
-		
+
 		StateDAOImplementr implementr=new StateDAOImplementr();
-		implementr.save("Dharwad");
-		implementr.save("Bagalkot");
-		implementr.save("Bidar");
-		implementr.save("Belagavi");
-		implementr.save("Bengalore");
-		implementr.save("Gadag");
-		implementr.save("Haveri");
-		implementr.save("Davangere");
-		implementr.save("Hassan");
-		implementr.save("Dakshin Kannada");
-		implementr.save("Uttar Kannada");
-		implementr.save("Tumakuru");
-		implementr.save("vijayapura");
-		implementr.save("Yadgi");
-		implementr.save("Mysore");
-		implementr.save("Kodagu");
-		implementr.save("Kolar");
-		implementr.save("Raichur");
-		implementr.save("Koppal");
-		implementr.save("Mandya");
+		implementr.save("Assam");
+		implementr.save("Bihar");
+		implementr.save("Chattisghar");
+		implementr.save("Karnataka");
+		implementr.save("Goa");
+		implementr.save("Gujarat");
+		implementr.save("Haryana");
+		implementr.save("Himachal Pradesh");
+		implementr.save("Bihar");
+		implementr.save("Jammu Kashmir");
+		implementr.save("Jharkhand");
+		implementr.save("Kerala");
+		implementr.save("Maharastra");
+		implementr.save("Manipur");
+		implementr.save("Nagaland");
+		implementr.save("Odisa");
+		implementr.save("Punjab");
+		implementr.save("Rajastan");
+		implementr.save("Tamil Nadu");
+		implementr.save("UttaraKhand");
 		
 		implementr.printlnAllStates();
 		
-		
-
 	}
 
 }
